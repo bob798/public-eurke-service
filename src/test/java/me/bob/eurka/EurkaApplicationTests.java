4b4cd0a810d5ab51c0f5da8a55d699ff0fe0c045
Bob <liubo@jinjiedao.com>
2020-03-28 14:21:25 +0800

    create public eurke

---
+++ src/test/java/me/bob/eurka/EurkaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bob.eurka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
